077af9d43f2748dd9380f3a259086fc9cd242430
monika24794 <33451089+monika24794@users.noreply.github.com>
2024-06-24 17:25:46 +0530

    Add files via upload

---
+++ FirstWebAppMVC1/Controllers/CustomerController.cs
@@ -0,0 +1,86 @@
+﻿using BuinessLogic_Library;
+using FirstWebAppMVC.Models;
+using HelperLibery;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FirstWebAppMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerHelper helper= new CustomerHelper();
+        public IActionResult NewCustomer()
+        { 
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewCustomer(CustomerViewModel model)
+        {
+            CustomerBAL bal = new CustomerBAL();
+            bal.CustomerId =model.CustomerId;
+            bal.CustName = model.CustName;
+            helper.insertCustomer(bal);
+         
+           return RedirectToAction("Index");   
+             //return Content(" Record  added successfulll"); 
+            //to return  string on action
+        }
+        public IActionResult Index()
+        {
+           List<CustomerBAL> custlist = helper.GetCustomers();
+            // show all list of customer
+           List<CustomerViewModel> modellist = new List<CustomerViewModel>();
+            foreach (var item in custlist)
+            {
+                CustomerViewModel model = new CustomerViewModel();
+                model.CustomerId = item.CustomerId;
+                model.CustName=item.CustName;
+                modellist.Add(model);
+            }
+            return View(modellist);
+        }
+        public IActionResult DeleteCustomer(int id)
+        {
+            CustomerBAL bal = helper.FindCustomer(id);
+            CustomerViewModel model = new CustomerViewModel();
+            model.CustomerId = bal.CustomerId;
+            model.CustName = bal.CustName;
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult DeleteCustomer(int id,CustomerViewModel model)
+        {
+            helper.deleteCustomer(id);
+            
+            return RedirectToAction("Index");
+            // return Content(" Record deletead successfulll");
+        }
+        public IActionResult EditCustomer(int id)
+        {
+            CustomerBAL bal = helper.FindCustomer(id);
+            CustomerViewModel model = new CustomerViewModel(); 
+            model.CustomerId = bal.CustomerId;
+            model.CustName = bal.CustName;
+            return View(model);
+
+        }
+            [HttpPost]
+        public IActionResult EditCustomer( int id,CustomerViewModel model)
+        { 
+           CustomerBAL bal=new CustomerBAL();
+            bal.CustomerId=model.CustomerId;
+            bal.CustName=model.CustName;
+            helper.UpdateCustomer(id, bal);
+            return RedirectToAction("Index");
+        }
+        public IActionResult FindCustomer(int id)
+        {
+            CustomerBAL bal = helper.FindCustomer(id);
+            CustomerViewModel model=new CustomerViewModel();
+            model.CustomerId=bal.CustomerId;
+            model.CustName=bal.CustName;
+            return View(model);
+        }
+    }
+}
---
+++ FirstWebAppMVC1/Models/CustomerViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstWebAppMVC.Models
+{
+    public class CustomerViewModel
+    {
+
+        private int _CustId;
+
+        public int CustomerId
+        {
+            get { return _CustId; }
+            set { _CustId = value; }
+        }
+        private String _custName;
+
+        public String CustName
+        {
+            get { return _custName; }
+            set { _custName = value; }
+        }
+
+    }
+}
